80e30a193b1dd0d412c30042850df324eb9d4f16
Teerapong Dinon <tp.dinon@gmail.com>
2019-02-05 19:40:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var arr = [];
+  for (var i = 0; i < musicians.length; i++) {
+    arr.push(musicians[i] + " plays " + instruments[i])
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  let n = 0
+  let arr1 = [];
+  while(n < facts.length){
+    arr1.push(facts[n] + '!!!')
+  }
+  return arr1;
+}
